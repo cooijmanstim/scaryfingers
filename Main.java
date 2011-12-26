02896696280f179e5296d033b50478aebc7fc663
Tim Cooijmans <t.cooijmans@student.maastrichtuniversity.nl>
2011-12-26 13:13:03 +0100

    move Program into its own file

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main {
 	public static void main(String[] args) {
-		Compressor.test();
-		
+		Program.test();
+
 		Compressor c = new Compressor();
 
-		Compressor.Program p;
+		Program p;
 
 		p = c.compress(new byte[]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
 		System.out.println(p);
@@ -17,7 +17,8 @@ public class Main {
 
 		p = c.compress(new byte[]{ 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 89 });
 		System.out.println(p);
-		
+
+		// ambitious indeed
 		p = c.compress("Hello World!\n".getBytes());
 		System.out.println(p);
 	}
